09489a028f6c344284da128c4e4a43f72fdef142
Saulo Palácio <saulo.hebert@gmail.com>
2024-08-27 00:26:47 -0300

    Desafio Decodificador Oracle Alura

--- main.js
+++ main.js
@@ -1,31 +1,52 @@
 var texto = document.getElementById("texto__digitado");
 var textoSaida = document.getElementById('saida').innerHTML;
 
-function encript() {    
+function encript() {  
+
     var resultado = texto.value
+
+    if(texto.value.trim() !== ''){
+
 
     resultado = resultado.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, "ai").replace(/o/g, 'ober').replace(/u/g, 'ufat');  
     
-    //document.querySelector('.saida').innerHTML = '<textarea readonly id="textSaida">' + resultado + '</textarea>';
+    
     document.getElementById('saida').innerHTML = 
 '<textarea readonly id="texto2">' + resultado + '</textarea>' + '<div class="botoes2">' +
-'<button class="btncopiar" id="copiar" onclick="copiar()">Copiar</button>' + '<button class="btncopiar" id="copiar" onclick="limpar()">Limpar</button>' + '</div>';
-    
+'<button class="btncopiar" id="copiar" onclick="copiar()">Copiar</button>' + '</div>';
     event.preventDefault();
+    }
+    else{
+        alert("Insira um texto a ser codificado");
+        event.preventDefault();
+    }    
 }
 
 function decrip() { 
     var resultado = texto.value; 
 
+    if(texto.value.trim() !== ''){
+
     resultado = resultado.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, "a").replace(/ober/g, 'o').replace(/ufat/g, 'u');
 
     document.getElementById('saida').innerHTML = 
 '<textarea readonly id="texto2">' + resultado + '</textarea>' + '<div class="botoes2">' +
-'<button class="btncopiar" id="copiar" onclick="copiar()">Copiar</button>' + '<button class="btncopiar" id="copiar" onclick="limpar()">Limpar</button>' + '</div>';
+'<button class="btncopiar" id="copiar" onclick="copiar()">Copiar</button>' + '</div>';
 
     event.preventDefault();
+    }
+    else{
+        alert("Insira um texto a ser decodificado");
+        event.preventDefault();
+    }
 }
 
 function limpar() {
     document.getElementById('saida').innerHTML = textoSaida;
+}
+
+function copiar(){
+    navigator.clipboard.writeText(document.getElementById('texto2').value);
+
+    alert("copiado");
 }
